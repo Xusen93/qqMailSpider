2527d2a2d2adaab87d98c97f40f9a781d4a9184d
didiaode <xusenthu@qq.com>
2017-06-29 16:36:55 +0800

    enrich functions

--- qqMailSpider/qqMailSpider.py
+++ qqMailSpider/qqMailSpider.py
@@ -1,13 +1,17 @@
 # -*- coding: utf-8 -*-
 # @Author: Xusen
+# @Email:  xusenthu@qq.com
 # @Date:   2017-06-26 11:28:40
 # @Last Modified by:   Xusen
-# @Last Modified time: 2017-06-29 13:16:27
+# @Last Modified time: 2017-06-29 16:36:45
 from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
 import logging
 import re
 import time
+import json
+import os
+import thulac
 
 
 logging.basicConfig(
@@ -19,13 +23,20 @@ class QQMailSpider(object):
     def __init__(self):
         self.CHROMEDRIVE = r'C:\Program Files (x86)\Google\Chrome\Application\chromedriver.exe'
         self.options = webdriver.ChromeOptions()
-        self.browser = webdriver.Chrome(
-            self.CHROMEDRIVE, chrome_options=self.options)
-        self.browser.maximize_window()
         self.mail_sender = []
         self.mail_sender_addr = []
         self.mail_subject = []
         self.mail_time = []
+
+    def run(self):
+        logging.info("启动QQ邮箱爬虫...")
+        self.browser = webdriver.Chrome(
+            self.CHROMEDRIVE, chrome_options=self.options)
+        self.browser.maximize_window()
+        self.login()
+        self.mailInfo()
+        self.logout()
+        self.info2json()
 
     def login(self):
         self.browser.get('https://mail.qq.com/')
@@ -37,7 +48,8 @@ class QQMailSpider(object):
 
     def logout(self):
         self.browser.switch_to_default_content()
-        self.browser.find_element_by_xpath("//*[@id='SetInfo']/div[1]/a[@target='_parent']").click()
+        self.browser.find_element_by_xpath(
+            "//*[@id='SetInfo']/div[1]/a[@target='_parent']").click()
         self.browser.quit()
         logging.info("已注销，浏览器退出！")
 
@@ -70,14 +82,33 @@ class QQMailSpider(object):
                         "//div/input[@class='btn']").click()
         logging.info("邮件信息读取完毕！")
 
+    def info2json(self):
+        jslist = []
+        for i in range(0, len(self.mail_sender)):
+            jslist.append({
+                'sender': self.mail_sender[i],
+                'from': self.mail_sender_addr[i],
+                'time': self.mail_time[i],
+                'subject': self.mail_subject[i],
+            })
+            self.json = json.dumps(jslist)
+        with open('info.json', 'w') as f:
+            f.write(self.json)
+        logging.info("邮件信息已存至 info.json 中")
+
+    def json2info(self):
+        pass
 
     def __switch_to_iframe(self, iframe_name, timeout=10):
         WebDriverWait(self.browser, timeout).until(
             lambda driver: driver.find_element_by_id(iframe_name).is_displayed())
         self.browser.switch_to.frame(iframe_name)
 
+
+def wordSplit():
+    pass
+
 if __name__ == '__main__':
     spider = QQMailSpider()
-    spider.login()
-    spider.mailInfo()
-    spider.logout()
+    if not os.path.exists('info.json'):
+        spider.run()
