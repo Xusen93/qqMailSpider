f0dc93b64b4f9ed5e0f2e38d654b9f426f1aa5f3
didiaode <xusenthu@qq.com>
2017-06-29 13:18:14 +0800

    enrich functions

--- qqMailSpider/qqMailSpider.py
+++ qqMailSpider/qqMailSpider.py
@@ -2,7 +2,7 @@
 # @Author: Xusen
 # @Date:   2017-06-26 11:28:40
 # @Last Modified by:   Xusen
-# @Last Modified time: 2017-06-28 22:26:42
+# @Last Modified time: 2017-06-29 13:16:27
 from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
 import logging
@@ -29,23 +29,7 @@ class QQMailSpider(object):
 
     def login(self):
         self.browser.get('https://mail.qq.com/')
-        logging.info("等待登陆")
-        # self.browser.switch_to.frame('login_frame')
-        # self.browser.find_element_by_xpath(
-        #     "//div/a[@id='switcher_plogin']").click()
-        # self.username = input("Please input your email count:")
-        # self.browser.find_element_by_xpath(
-        #     "//input[@id='u']").send_keys(self.username)
-        # self.passwd = input("Please input the password:")
-        # self.browser.find_element_by_xpath(
-        #     "//input[@id='p']").send_keys(self.passwd)
-        # if self.browser.find_element_by_xpath("//*[@id='verifyArea']").is_displayed():
-        #     self.verifycode = input("Please input the verifycode:")
-        #     self.browser.find_element_by_xpath(
-        #         "//input[@id='verifycode']").send_keys(self.verifycode)
-        # time.sleep(1)
-        # # self.browser.find_element_by_xpath(
-        # #     "//input[@id='login_button']").click()
+        logging.info("等待登陆，请进入浏览器页面登陆QQ邮箱")
         WebDriverWait(self.browser, 1000).until(
             lambda driver: driver.find_element_by_id('readmailbtn_link').is_displayed())  # 等待登陆完成
         self.browser.find_element_by_xpath(
@@ -55,7 +39,7 @@ class QQMailSpider(object):
         self.browser.switch_to_default_content()
         self.browser.find_element_by_xpath("//*[@id='SetInfo']/div[1]/a[@target='_parent']").click()
         self.browser.quit()
-        logging.info("注销登陆，浏览器退出！")
+        logging.info("已注销，浏览器退出！")
 
     def mailInfo(self):
         self.__switch_to_iframe('mainFrame')
